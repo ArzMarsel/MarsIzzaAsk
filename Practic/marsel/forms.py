ad2b15651b954d7c55f1a4b2417fd4118e05c44e
bd-izzat <izzat.zakirov2001@gmail.com>
2024-03-30 14:49:35 +0600

    commit register&login

---
+++ Practic/marsel/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django_recaptcha.fields import ReCaptchaField
+
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    captcha = ReCaptchaField()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+    captcha = ReCaptchaField()
+
--- Practic/marsel/views.py
+++ Practic/marsel/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
+from .forms import RegistrationForm, LoginForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            User.objects.create_user(username=username, email=email, password=password)
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('dashboard')
+            else:
+                # Если пользователь с такими учетными данными не найден
+                messages.error(request, 'Неверное имя пользователя или пароль. Пожалуйста, попробуйте снова.')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
