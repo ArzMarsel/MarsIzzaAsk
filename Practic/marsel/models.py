76595030ace03b9b83a7ccc00fabd569a77dde77
bd-izzat <izzat.zakirov2001@gmail.com>
2024-03-30 15:55:34 +0600

    commit teacher_login

--- Practic/marsel/models.py
+++ Practic/marsel/models.py
@@ -11,3 +11,21 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+
+class Exercise(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField('Название задания:', max_length=50)
+    description = models.TextField('Описание задания:')
+    deadline = models.DateTimeField('Срок сдачи:')
+
+    def __str__(self):
+        return self.title
+
+
+class Solution(models.Model):
+    assignment = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    student_name = models.CharField('Имя студента:', max_length=50)
+    file = models.FileField(upload_to='solutions/')
+
+    def __str__(self):
+        return f"Решение для задания '{self.assignment.title}' от {self.student_name}"
