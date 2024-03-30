60f43911fc17fed2ddb057cb3846d2f535edd121
ArzMarsel <marselarzybekov3@gmail.com>
2024-03-30 14:51:13 +0600

    Marsel4

--- Practic/marsel/models.py
+++ Practic/marsel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField('Название:', max_length=20)
+    description = models.TextField('Описание:')
+    date1 = models.DateField('Дата начала:')
+    date2 = models.DateField("Дата окончания:")
+    teachers = models.TextField('Учителя:')
+
+    def __str__(self):
+        return self.title
+
