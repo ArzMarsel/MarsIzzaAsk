9afab29e2fd52ecf4a7aadc252d0f078056c81af
ArzMarsel <marselarzybekov3@gmail.com>
2024-04-04 19:54:36 +0600

    Marsel8

--- Practic/Practic/settings.py
+++ Practic/Practic/settings.py
@@ -134,3 +134,6 @@ RECAPTCHA_PRIVATE_KEY = '6Lf3YaEpAAAAAPcjyRaetP7bI9xt07u7h9eyqWLu'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'dashboard'
